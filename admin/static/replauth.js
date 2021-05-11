4d07f89398770f6ebce6459749d259cf329bb1ff
frissyn <62220201+frissyn@users.noreply.github.com>
2021-05-11 11:54:40 +0000

    💄: beautify replauth script

--- admin/static/replauth.js
+++ admin/static/replauth.js
@@ -1,45 +1,46 @@
-;(function() {
-var selem = document.currentScript;
+;
+(function() {
+    var selem = document.currentScript;
 
-var button = document.createElement('button');
-button.className = "button is-primary replit-auth-button";
-button.textContent = 'Login with Replit';
+    var button = document.createElement('button');
+    button.className = "button is-primary replit-auth-button";
+    button.textContent = 'Login with Replit';
 
-if (location.protocol !== 'https:') {
-var err = document.createElement('div');
-err.className = "replit-auth-error";
-err.textContent = 'Replit auth requires https!';
-selem.parentNode.insertBefore(err, selem);
-}
+    if (location.protocol !== 'https:') {
+        var err = document.createElement('div');
+        err.className = "replit-auth-error";
+        err.textContent = 'Replit auth requires https!';
+        selem.parentNode.insertBefore(err, selem);
+    }
 
-button.onclick = function() {
-window.addEventListener('message', authComplete);
+    button.onclick = function() {
+        window.addEventListener('message', authComplete);
 
-var h = 500;
-var w = 350;
-var left = (screen.width / 2) - ( w / 2);
-var top = (screen.height / 2) - (h / 2);
+        var h = 500;
+        var w = 350;
+        var left = (screen.width / 2) - (w / 2);
+        var top = (screen.height / 2) - (h / 2);
 
-var authWindow = window.open(
-'https://repl.it/auth_with_repl_site?domain='+location.host,
-'_blank',
-'modal =yes, toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left)
+        var authWindow = window.open(
+            'https://repl.it/auth_with_repl_site?domain=' + location.host,
+            '_blank',
+            'modal =yes, toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
 
-function authComplete(e) {
-if (e.data !== 'auth_complete') {
-	return;
-}
+        function authComplete(e) {
+            if (e.data !== 'auth_complete') {
+                return;
+            }
 
-window.removeEventListener('message', authComplete);
+            window.removeEventListener('message', authComplete);
 
-authWindow.close();
-if (selem.attributes.authed.value) {
-	eval(selem.attributes.authed.value);
-} else {
-	location.reload();
-}
-}
-}
+            authWindow.close();
+            if (selem.attributes.authed.value) {
+                eval(selem.attributes.authed.value);
+            } else {
+                location.reload();
+            }
+        }
+    }
 
-selem.parentNode.insertBefore(button, selem);
+    selem.parentNode.insertBefore(button, selem);
 })();
